3d6a790c7810f2d843c820b9a0ed3528a6a2ff41
Abhishek911cse <61340365+Abhishek911cse@users.noreply.github.com>
2020-10-26 18:36:00 +0530

    Add form interface and folder organisation

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+
+# @app.route("/result", methods=["POST", "GET"])
+# def result():
+#     if request.method == "POST":
+#         result = request.form
+#         return render_template("result.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

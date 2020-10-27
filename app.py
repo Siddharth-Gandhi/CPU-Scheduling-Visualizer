d07bea8b2ed06717f18cc12449377042b50e2976
Siddharth Gandhi <ssgandhi602@gmail.com>
2020-10-27 11:13:33 +0530

    Remove gifs from gitignore and fix app.py

--- app.py
+++ app.py
@@ -27,17 +27,10 @@ def fcfs():
         print(burst)
         algoName = request.form["algo"]
         if algoName == "FCFS":
-<<<<<<< HEAD
             pr_no, arrival, burst = FCFS.sort_by_arrival(
                 pr_no, arrival, burst, n)
-            FCFS.findAllTimes(pr_no, arrival, burst, n)
-            FCFS.plot(pr_no, arrival, burst, n)
-        # return render_template(
-        #     "temp.html", pr_no=pr_no, arrival=arrival, burst=burst, algoName=algoName
-        # )
-=======
-            pr_no, arrival, burst = FCFS.sort_by_arrival(pr_no, arrival, burst, n)
-            wait, TAT, comp, avgWT, avgTAT = FCFS.findAllTimes(pr_no, arrival, burst, n)
+            wait, TAT, comp, avgWT, avgTAT = FCFS.findAllTimes(
+                pr_no, arrival, burst, n)
             FCFS.plot(pr_no, arrival, burst, n)
         return render_template(
             "fcfs.html",
@@ -51,8 +44,6 @@ def fcfs():
             avgWT=avgWT,
             avgTAT=avgTAT,
         )
->>>>>>> 1318ebfbdd7e3d27640ee6a1f15006c39c643605
-
 
 # @app.route("/result", methods=["POST", "GET"])
 # def result():
